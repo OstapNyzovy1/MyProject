569c80e5814d00ae5d58e2d19ba2534d73c396ad
Study <Study@PC-5-604>
2016-08-02 17:08:48 +0300

    grg

--- Test/src/ua/Main.java
+++ Test/src/ua/Main.java
@@ -5,9 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int[] array = new int[10];
-		insert(array);
+
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
+			System.out.println();
+			System.out.println();
 		}
 
 	}
