d5ce02653ce40486f484e23127c3fb8a11d9cb7f
Study <Study@PC-5-604>
2016-08-02 17:00:45 +0300

    commit

--- Test/src/ua/Main.java
+++ Test/src/ua/Main.java
@@ -3,8 +3,13 @@ package ua;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		String n = "Vasyl";
+		int i = 123123;
+		
 		System.out.println("Hello");
 		System.out.println("GitHub");
+		
 	}
 
 }
