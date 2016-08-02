7d0d4fc8d6333c3fba2472298a256a2f5ffa9437
Study <Study@PC-5-604>
2016-08-02 17:02:35 +0300

    commit

--- Test/src/ua/Main.java
+++ Test/src/ua/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int[] array = new int[10];
+		insert(array);
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 
 	}
 
